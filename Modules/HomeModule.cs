a7d4ecdbf6823cbd18612532bf51ae9f3ca9458a
Alyssa Moody and David Wilson <alyssa+david@>
2017-06-07 16:39:00 -0700

    today was a great success.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,6 +13,44 @@ namespace Restaurants
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Get["/cuisines"] = _ => {
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["cuisines.cshtml", allCuisines];
+      };
+      Get["/cuisines/new"] = _ => {
+        return View["cuisine_add.cshtml"];
+      };
+      Post["/cuisines/new"] = _ => {
+        Cuisine newCuisine = new Cuisine(Request.Form["cuisine-type"], Request.Form["cuisine-description"]);
+        newCuisine.Save();
+        return View["success.cshtml"];
+      };
+      Get["/restaurants"] = _ => {
+        List<Restaurant> allRestaurants = Restaurant.GetAll();
+        return View["restaurants.cshtml", allRestaurants];
+      };
+      Get["/restaurants/new"] = _ => {
+        List<Cuisine> allCuisines = Cuisine.GetAll();
+        return View["restaurant_add.cshtml", allCuisines];
+      };
+      Post["/restaurants/new"] = _ => {
+        Restaurant newRestaurant = new Restaurant(Request.Form["restaurant-name"], Request.Form["restaurant-rating"], Request.Form["cuisine-id"]);
+        newRestaurant.Save();
+        return View["success.cshtml"];
+      };
+      Get["/reviews"] = _ => {
+        List<Review> allReviews = Review.GetAll();
+        return View["reviews.cshtml", allReviews];
+      };
+      Get["/reviews/new"] = _ => {
+        List<Restaurant> allRestaurants = Restaurant.GetAll();
+        return View["review_add.cshtml", allRestaurants];
+      };
+      Post["/reviews/new"] = _ => {
+        Review newReview = new Review(Request.Form["review-UserName"], Request.Form["review-score"], Request.Form["review-comment"], Request.Form["restaurant-id"]);
+        newReview.Save();
+        return View["success.cshtml"];
+      };
     }
   }
 }
